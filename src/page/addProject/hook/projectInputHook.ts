02bfe8ac3421f53e08cf146562d01235db13d023
nikasakana <nikasakana@gmail.com>
2022-04-13 22:42:38 +0400

    added custom hook for project registration

---
+++ src/page/addProject/hook/projectInputHook.ts
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addProject, AddProject } from "../../../state/reducer/addProjectSlice";
+import {
+  filterTechnology,
+  getAllTechnologiesObj,
+} from "../../../state/reducer/technologySlice";
+import { RootState } from "../../../state/store";
+
+export const useAddProjectInputs = () => {
+  const [addingProject, setAddingProject] = useState(false);
+  const [inputs, setInputs] = useState<any>({
+    name: "",
+    name_error: "",
+    description: "",
+    description_error: "",
+    start_date: "",
+    start_date_error: "",
+    github_url: "",
+    github_url_error: "",
+    url: "",
+    url_error: "",
+    technology_ids: [1],
+    technology_ids_error: "",
+  });
+  const { token }: { token: string | undefined } = useSelector(
+    (state: RootState) => state.user
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (addingProject) {
+      const valid = validateInputs();
+      if (valid) {
+        const proj = {
+          title: inputs.name,
+          description: inputs.description,
+          start_date: inputs.start_date,
+          github_url: inputs.github_url,
+          technology_ids: inputs.technology_ids,
+          url: inputs.url,
+          stars: 1,
+        } as AddProject;
+        dispatch(addProject({ project: proj, token }));
+      }
+      setAddingProject(false);
+    }
+  }, [addingProject]);
+
+  useEffect(() => {
+    dispatch(filterTechnology(getAllTechnologiesObj()));
+  }, []);
+
+  const validateInputs = () => {
+    let valid = true;
+    if (inputs.name === "") {
+      setInputs((prev: any) => ({ ...prev, name_error: "Please input Name" }));
+      valid = false;
+    } else {
+      setInputs((prev: any) => ({ ...prev, name_error: "" }));
+    }
+    if (inputs.description === "") {
+      setInputs((prev: any) => ({
+        ...prev,
+        description_error: "Please input Description",
+      }));
+      valid = false;
+    } else {
+      setInputs((prev: any) => ({ ...prev, description_error: "" }));
+    }
+    if (inputs.start_date === "") {
+      setInputs((prev: any) => ({
+        ...prev,
+        start_date_error: "Please input Start date",
+      }));
+      valid = false;
+    } else {
+      setInputs((prev: any) => ({ ...prev, start_date_error: "" }));
+    }
+    if (inputs.github_url === "") {
+      setInputs((prev: any) => ({
+        ...prev,
+        github_url_error: "Invalid URL",
+      }));
+      valid = false;
+    } else {
+      setInputs((prev: any) => ({ ...prev, github_url_error: "" }));
+    }
+    if (inputs.url === "") {
+      setInputs((prev: any) => ({
+        ...prev,
+        url_error: "Invalid URL",
+      }));
+      valid = false;
+    } else {
+      setInputs((prev: any) => ({ ...prev, url_error: "" }));
+    }
+    if (inputs.technology_ids.length == 0) {
+      setInputs((prev: any) => ({
+        ...prev,
+        technology_ids_error: "Invalid technology ids",
+      }));
+      valid = false;
+    } else {
+      setInputs((prev: any) => ({ ...prev, technology_ids_error: "" }));
+    }
+    return valid;
+  };
+
+  return {
+    inputs,
+    setAddingProject,
+    setInputs,
+  };
+};
--- src/page/addProject/screen/AddProjectScreen.tsx
+++ src/page/addProject/screen/AddProjectScreen.tsx
@@ -1,119 +1,29 @@
-import { Text, StyleSheet, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import React from "react";
 import { color } from "../../../commons/style";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  filterTechnology,
-  getAllTechnologiesObj,
-} from "../../../state/reducer/technologySlice";
+import { useSelector } from "react-redux";
+import { TechnologyState } from "../../../state/reducer/technologySlice";
 import { RootState } from "../../../state/store";
-import { TechnologyData } from "../../../commons/model/index";
 import Loading from "../../../commons/component/LoadingView";
 import Input from "../../../commons/component/InputView";
 import TitleView from "../../../commons/component/TitleView";
 import Button from "../../../commons/component/ButtonView";
-import { AddProject, addProject } from "../../../state/reducer/addProjectSlice";
+import { useAddProjectInputs } from "../hook/projectInputHook";
+import { AddProjectState } from "../../../state/reducer/addProjectSlice";
 
 const AddProjectScreen = () => {
-  const dispatch = useDispatch();
-  const { technologies }: { technologies: TechnologyData[]; loading: string } =
-    useSelector((state: RootState) => state.technologies);
-  const { loading }: { loading: string } = useSelector(
-    (state: RootState) => state.addProject
-  );
-  const { token }: { token: string | undefined } = useSelector(
-    (state: RootState) => state.user
-  );
-  useEffect(() => {
-    dispatch(filterTechnology(getAllTechnologiesObj()));
-  }, []);
+  const { setAddingProject, setInputs, inputs } = useAddProjectInputs();
 
-  const [inputs, setInputs] = useState<any>({
-    name: "",
-    name_error: "",
-    description: "",
-    description_error: "",
-    start_date: "",
-    start_date_error: "",
-    github_url: "",
-    github_url_error: "",
-    url: "",
-    url_error: "",
-    technology_ids: [1],
-    technology_ids_error: "",
-  });
-
-  const addProjectCallback = () => {
-    let valid = true;
-    if (inputs.name === "") {
-      setInputs((prev: any) => ({ ...prev, name_error: "Please input Name" }));
-      valid = false;
-    } else {
-      setInputs((prev: any) => ({ ...prev, name_error: "" }));
-    }
-    if (inputs.description === "") {
-      setInputs((prev: any) => ({
-        ...prev,
-        description_error: "Please input Description",
-      }));
-      valid = false;
-    } else {
-      setInputs((prev: any) => ({ ...prev, description_error: "" }));
-    }
-    if (inputs.start_date === "") {
-      setInputs((prev: any) => ({
-        ...prev,
-        start_date_error: "Please input Start date",
-      }));
-      valid = false;
-    } else {
-      setInputs((prev: any) => ({ ...prev, start_date_error: "" }));
-    }
-    if (inputs.github_url === "") {
-      setInputs((prev: any) => ({
-        ...prev,
-        github_url_error: "Invalid URL",
-      }));
-      valid = false;
-    } else {
-      setInputs((prev: any) => ({ ...prev, github_url_error: "" }));
-    }
-    if (inputs.url === "") {
-      setInputs((prev: any) => ({
-        ...prev,
-        url_error: "Invalid URL",
-      }));
-      valid = false;
-    } else {
-      setInputs((prev: any) => ({ ...prev, url_error: "" }));
-    }
-    if (inputs.technology_ids.length == 0) {
-      setInputs((prev: any) => ({
-        ...prev,
-        technology_ids_error: "Invalid technology ids",
-      }));
-      valid = false;
-    } else {
-      setInputs((prev: any) => ({ ...prev, technology_ids_error: "" }));
-    }
-    if (valid) {
-      const proj = {
-        title: inputs.name,
-        description: inputs.description,
-        start_date: inputs.start_date,
-        github_url: inputs.github_url,
-        technology_ids: inputs.technology_ids,
-        url: inputs.url,
-        stars: 1,
-      } as AddProject;
-      dispatch(addProject({ project: proj, token }));
-    }
-  };
+  const {
+    technologies, //todo: use this array for tech tags
+    addProject,
+  }: { technologies: TechnologyState; addProject: AddProjectState } =
+    useSelector((state: RootState) => state);
 
   return (
     <SafeAreaView style={styles.container}>
-      {loading === "pending" ? (
+      {addProject.loading === "pending" ? (
         <Loading />
       ) : (
         <>
@@ -162,13 +72,13 @@ const AddProjectScreen = () => {
           </View> */}
           <Button
             callback={() => {
-              addProjectCallback();
+              setAddingProject(true);
             }}
             text={"Create project"}
           />
-          {loading === "succeeded" ? (
+          {addProject.loading === "succeeded" ? (
             <TitleView text={"Succesfully added project"} />
-          ) : loading === "failed" ? (
+          ) : addProject.loading === "failed" ? (
             <TitleView text={"Failed to added project"} />
           ) : null}
         </>
